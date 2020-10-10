64371860484b33a5396250c503e96a42a598ee27
东风 赵 <zdf@cheok.com>
2020-10-10 10:06:31 +0800

    添加项目文件。

---
+++ Course1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Course1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Course1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Course1.WebApiApplication" Language="C#" %>
