990ba332eafee0bd1dfd5a7e1c17e56eba0ac5bc
东风 赵 <zdf@cheok.com>
2021-01-22 17:44:04 +0800

    代码添加

--- Course05/ThredModel/OneThreds.cs
+++ Course05/ThredModel/OneThreds.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Course05.Model.ThredModel
 {
     public class OneThreds
     {
+        //进程 ： 计算机概念，程序在服务器运行时占据全部计算资源总和 虚拟得
+        // 线程 ： 计算机概念 进程在响应操作时最小单位，也包含cpu内存 网络 硬盘 IO
+        //一个进程会包含多个线程，线程隶属于某个进程,进程销毁 线程也销毁
+        //句柄 ： 其实是个 Long 数字 是操作系统标识应用程序
 
+        //线程 ： 计算机概念 一个进程有多个线程同时进行
+
+        public static void OneRunThread() 
+        {
+            Task
+        }
     }
 }
---
+++ TwoWinForm/OneModel/OneModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoWinForm.OneModel
+{
+    public class OneModel
+    {
+
+    }
+}
