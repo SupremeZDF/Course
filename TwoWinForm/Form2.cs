80db13773dffb89554c6851f199b41caf2093ee4
东风 赵 <zdf@cheok.com>
2021-02-05 17:45:09 +0800

    代码提交

--- TwoWinForm/Form2.cs
+++ TwoWinForm/Form2.cs
@@ -37,5 +37,10 @@ namespace TwoWinForm
 
             TwoThreadExercise.OneTaskExercse();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            TwoThreadExercise.OneTaskExercse();
+        }
     }
 }
